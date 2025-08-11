39137c6beb45011f8d055d3be17d43d7c7f41c28
supakkit <87434100+supakkit@users.noreply.github.com>
2025-08-11 10:26:27 +0700

    added new views

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./views/Layout";
 import { Home } from "lucide-react";
-
-
+import { Favorite } from "./views/Favorite";
+import { Cart } from "./views/Cart";
+import { Checkout } from "./views/Checkout";
 
 const router = createBrowserRouter([
   {
@@ -13,11 +14,21 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />
       },
+      {
+        path: 'favorite',
+        element: <Favorite />
+      },
+      {
+        path: 'cart',
+        element: <Cart />
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />
+      },
     ]
   }
 ]);
-
-
 
 export default function App() {
   return <RouterProvider router={router} />
---
+++ src/views/Favorite.jsx
@@ -0,0 +1,5 @@
+export function Favorite() {
+    return (
+        <div>Favorite</div>
+    );
+}
